9b8f185c384ae53cda1929e45321701f8c3fb166
Dnadapierre <photostudio13000@gmail.com>
2022-07-27 15:58:16 +0200

    ajout du projet sortir

---
+++ src/Controller/Admin/EventCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Event;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+
+class EventCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Event::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        yield TextField::new('title');
+        yield TextField::new('content');
+        yield AssociationField::new('campus');
+        yield DateField::new('beginAt');
+        yield DateField::new('endAt');
+    }
+    
+}
