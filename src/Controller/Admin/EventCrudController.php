9ea1106baf3e10ab0b39037885731478af6b43d7
Dnadapierre <photostudio13000@gmail.com>
2022-07-28 12:31:27 +0200

    modif admin crud event

--- src/Controller/Admin/EventCrudController.php
+++ src/Controller/Admin/EventCrudController.php
@@ -6,6 +6,7 @@ use App\Entity\Event;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 
 class EventCrudController extends AbstractCrudController
@@ -18,11 +19,13 @@ class EventCrudController extends AbstractCrudController
     
     public function configureFields(string $pageName): iterable
     {
+        yield AssociationField::new('user');
         yield TextField::new('title');
         yield TextField::new('content');
         yield AssociationField::new('campus');
         yield DateField::new('beginAt');
         yield DateField::new('endAt');
+        yield BooleanField::new('isActive');
     }
     
 }
