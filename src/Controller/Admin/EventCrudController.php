250e4df6017bbf7f0f33446063e062843cf9e579
Dnadapierre <photostudio13000@gmail.com>
2022-08-29 15:49:27 +0200

    création VichImageField custom field et ajout au crud admin

--- src/Controller/Admin/EventCrudController.php
+++ src/Controller/Admin/EventCrudController.php
@@ -2,13 +2,16 @@
 
 namespace App\Controller\Admin;
 
+
+use VichImageField;
 use App\Entity\Event;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class EventCrudController extends AbstractCrudController
 {
@@ -24,6 +27,8 @@ class EventCrudController extends AbstractCrudController
         yield TextField::new('title');
         yield TextEditorField::new('content');
         yield AssociationField::new('campus');
+        yield ImageField::new('imageName')->setBasePath('images/soiree')->onlyOnIndex();
+        yield VichImageField::new('imageFile')->hideOnIndex();
         yield DateTimeField::new('beginAt');
         yield DateTimeField::new('endAt');
         yield AssociationField::new('place');
